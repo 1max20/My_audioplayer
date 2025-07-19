477865ff0cdaf51c90ac7fecc691cd5825eeb722
1max20 <1max12sid@mail.ru>
2025-07-19 21:11:54 +0300

    Добавил .gitignore для исключения временных и сборочных файлов

--- My_player/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_My_player_FILE A:/Pet_project/My_player/My_player/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/My_player.exe)
-set(__QT_DEPLOY_TARGET_My_player_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_My_player_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
